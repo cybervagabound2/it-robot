407af52a5b23a526021f52c2f2199b448d244e8e
brodyaga <cybervagabound2@protonmail.com>
2018-11-12 11:58:25 -0500

    some changes

--- main.py
+++ main.py
@@ -23,7 +23,8 @@ def main():
                           url_path='631771539:AAEYWzzk3HYbWXP9OAM3bklD56Z40H1K9dA',
                           key='/home/tester/webhook_pkey.pem',
                           cert='/home/tester/webhook_cert.pem',
-                          webhook_url='https://35.204.2.195:8443/631771539:AAEYWzzk3HYbWXP9OAM3bklD56Z40H1K9dA')
+                          webhook_url='https://35.204.2.195:8443/it-robot/631771539:AAEYWzzk3HYbWXP9OAM3bklD56Z40H1K9dA')
+
 
 
 if __name__ == '__main__':

56641ed888214ad594fb09c64890ad00ea4acfe0
brodyaga <cybervagabound2@protonmail.com>
2018-11-12 11:48:47 -0500

    main initialized

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import logging
-from telegram.ext improt (Updater, CommandHandler, Filters, CallbackQueryHandler)
+from telegram.ext import (Updater, CommandHandler, Filters, CallbackQueryHandler)
 from handlers.start import start
 
 
@@ -18,4 +18,13 @@ def main():
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('start', start))
     dp.add_error_handler(error)
-    
+    updater.start_webhook(listen='0.0.0.0',
+                          port='8443',
+                          url_path='631771539:AAEYWzzk3HYbWXP9OAM3bklD56Z40H1K9dA',
+                          key='/home/tester/webhook_pkey.pem',
+                          cert='/home/tester/webhook_cert.pem',
+                          webhook_url='https://35.204.2.195')
+
+
+if __name__ == '__main__':
+    main()
